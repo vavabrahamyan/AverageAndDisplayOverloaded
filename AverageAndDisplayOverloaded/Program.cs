562e1209cc7ff51e380fa0c1711ed629f764d874
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-12-24 09:49:30 +0400

    Добавьте файлы проекта.

---
+++ AverageAndDisplayOverloaded/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AverageAndDisplayOverloaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AverageAndDisplay("my GPA", 3.5, "your GPA", 4.0);
+            Console.WriteLine();
+
+            AverageAndDisplay(3.5, 4.0);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+
+        public static void AverageAndDisplay(string s1, double d1,
+                                             string s2, double d2)
+        {
+            double average = (d1 + d2) / 2;
+            Console.WriteLine("The average of " + s1
+                            + " whose value is " + d1);
+            Console.WriteLine("and " + s2
+                            + " whose value is " + d2
+                            + " is " + average);
+        }
+
+        public static void AverageAndDisplay(double d1, double d2)
+        {
+            double average = (d1 + d2) / 2;
+            Console.WriteLine("The average of " + d1
+                            + " and " + d2
+                            + " is " + average);
+        }
+    }
+}
